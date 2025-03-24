dd280913e2ab40aca86f978d7a1bdcda2b7ac10b
ilenisnerd <ilenisnerd@yandex.ru>
2025-03-24 22:53:02 +0300

    add main.yml deploy

--- infra/main.tf
+++ infra/main.tf
@@ -115,23 +115,11 @@ resource "yandex_compute_instance" "vm" {
 package_update: true
 package_upgrade: true
 
-# Установка зависимостей для добавления репозитория
 packages:
-  - apt-transport-https
-  - ca-certificates
-  - curl
-  - gnupg
-  - lsb-release
+  - docker.io
+  - docker-compose-plugin
 
-# Добавление GPG-ключа Docker
 runcmd:
-  - mkdir -p /etc/apt/keyrings
-  - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  - echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Установка Docker и плагина docker compose
-  - apt-get update
-  - apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   - systemctl enable docker
   - systemctl start docker
   - usermod -aG docker Ubuntu
